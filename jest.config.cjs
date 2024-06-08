3ab8251198bc995a7150c461fce0603b964457c7
hideki kuno <hidekuno@gmail.com>
2024-06-08 18:51:15 +0900

    modified babel to swc

---
+++ jest.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('@swc/core').Config} */
+const swcConfig = {
+  sourceMaps: true,
+  module: {
+    type: 'commonjs',
+  },
+  jsc: {
+    parser: {
+      syntax: 'typescript',
+      tsx: true,
+    },
+    transform: {
+      react: {
+        runtime: 'automatic',
+      },
+    },
+  },
+};
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest', swcConfig],
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  },
+};
