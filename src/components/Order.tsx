eed6c97d0b718383ecb36605b7e3e5cfcc4acdb1
hideki kuno <hidekuno@gmail.com>
2024-06-20 09:39:49 +0900

    fix build error

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -12,7 +12,6 @@ import TableCell, {tableCellClasses} from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
 import {ShopContext,OrderItem} from '../store';
 
